6e6f378c6d3fb121ac98dcf9a907986de4fc5efb
Архипов Владимир <arhipvp@gmail.com>
2023-09-11 23:17:15 +0300

    11.09.2023

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from model import core
+from model.database import engine, Base
 from routers.items import router as router_items
+
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
-app.include_router(
-    router=router_items,
-    prefix='/items',
-)
---
+++ settings.py
@@ -0,0 +1,4 @@
+POSTGRES_USER='postgres'
+POSTGRES_PASSWORD='postgres'
+POSTGRES_DB='postgres'
+SQLALCHEMY_DATABASE_URL='postgresql://postgres:postgres@localhost/postgres'
